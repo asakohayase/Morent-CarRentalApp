d52673ca96fbdec6cfee34a3057efd683d3f4916
Matthew Gajo <matthew.gajo@gmail.com>
2023-08-08 18:28:20 -0400

    folder/file structure layout & eslint & prettier

---
+++ components/placeholder.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const placeholder = (props: Props) => {
+  return <div>placeholder</div>;
+};
+
+export default placeholder;
---
+++ components/reusable/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+	<footer>Footer</footer>
+  )
+}
+
+export default Footer
