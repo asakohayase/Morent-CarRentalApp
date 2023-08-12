46693e620ed1d37cf69af59cce5402bb7c595f6f
Ryan Carpenter <scrian_12@yahoo.com>
2023-08-11 21:05:28 -0400

    fixed semantic elements

--- components/reusable/CarCard.tsx
+++ components/reusable/CarCard.tsx
@@ -17,16 +17,16 @@ const CarCard = ({
   data: { title, vehicleType, gallons, image, transmission, capacity, price },
 }: Props) => {
   return (
-    <div className="flex h-[240px] w-[327px] flex-col justify-between rounded-[10px] bg-white p-6 md:h-[388px] md:w-[304px]">
-      <div className="flex items-center justify-between">
-        <div className="flex-col">
+    <article className="flex h-[240px] w-[327px] flex-col justify-between rounded-[10px] bg-white p-6 md:h-[388px] md:w-[304px]">
+      <header className="flex items-center justify-between">
+        <section className="flex-col">
           <h1 className="text-base font-semibold text-gray-900 md:text-xl md:font-bold">
             {title}
           </h1>
           <p className="text-xs font-medium text-slate-400 md:text-sm md:font-bold">
             {vehicleType}
           </p>
-        </div>
+        </section>
         <Image
           src="/img/heart.svg"
           alt="heart"
@@ -34,8 +34,8 @@ const CarCard = ({
           width={16}
           className="mb-5 md:h-6 md:w-6"
         />
-      </div>
-      <div className="flex items-center justify-between  md:h-52 md:flex-col md:items-start">
+      </header>
+      <section className="flex items-center justify-between  md:h-52 md:flex-col md:items-start">
         <Image
           src={image}
           alt={title}
@@ -44,8 +44,8 @@ const CarCard = ({
           className="md:mt-8 md:h-[100px] md:w-[248px]"
         />
         <div className="absolute  top-[125px] z-10 h-[44px] w-[190px] bg-gradient-to-b from-white to-white opacity-20 md:top-[170px]  md:h-[68px] md:w-[250px]" />
-        <div className="flex  flex-col">
-          <div className="flex flex-col items-start justify-between  text-sm font-medium text-slate-400 md:w-[250px] md:flex-row">
+        <section className="flex  flex-col">
+          <section className="flex flex-col items-start justify-between  text-sm font-medium text-slate-400 md:w-[250px] md:flex-row">
             <div className="flex gap-2">
               <Image
                 src="/img/gas-station.svg"
@@ -68,10 +68,10 @@ const CarCard = ({
               <Image src="/img/capacity.svg" alt="gas" width={16} height={16} />
               <p>{capacity}</p>
             </div>
-          </div>
-        </div>
-      </div>
-      <div>
+          </section>
+        </section>
+      </section>
+      <section>
         <p>
           <span className="text-base font-bold text-gray-900 md:text-xl">
             {price}
@@ -80,8 +80,8 @@ const CarCard = ({
             day
           </span>
         </p>
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 
