460a97007f0fa7972356b7cccce626d1e661898e
Asako Hayase <chipmunk.kck@gmail.com>
2023-08-15 01:55:31 -0700

    updated semantic

--- app/editcar/page.tsx
+++ app/editcar/page.tsx
@@ -5,7 +5,7 @@ import Image from '@/node_modules/next/image';
 
 const page = () => {
   return (
-    <div className="padding-layout relative flex flex-col px-6 py-10 md:py-[30px]">
+    <section className="padding-layout relative flex flex-col px-6 py-10 md:py-[30px]">
         <div className="flex flex-col md:flex-row-reverse md:justify-between">
             <div className="mb-5 flex h-[89px] w-[327px] items-center justify-center rounded bg-neutral-100 md:mb-0 md:mr-[38px] md:h-[49px] md:w-[122px]">
                 <Image 
@@ -16,15 +16,15 @@ const page = () => {
                     />
             </div>
             <div className="inline-flex flex-col items-start justify-start gap-2.5">
-                <div className="text-xl font-bold text-gray-900">Edit Car Details</div>
-                <div className="text-sm font-medium text-gray-400">Please enter your car info</div>
+                <h1 className="text-xl font-bold text-gray-900">Edit Car Details</h1>
+                <h3 className="text-sm font-medium text-gray-400">Please enter your car info</h3>
             </div>
         </div>
         <div className="inline-flex flex-col items-start justify-start gap-6 pt-[34px]">
-            <div className="text-lg font-extrabold text-blue-500">CAR INFO</div>
+            <h3 className="text-lg font-extrabold text-blue-500">CAR INFO</h3>
             <EditCarForm />
         </div>
-   </div>
+   </section>
   )
 }
 
