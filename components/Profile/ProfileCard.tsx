880f02f0652da13690f24a5dde7bfeb2f20f6ee4
Asako Hayase <chipmunk.kck@gmail.com>
2023-08-19 12:24:15 -0700

    update: fix styling for button and added my cars for rent section

--- components/Profile/ProfileCard.tsx
+++ components/Profile/ProfileCard.tsx
@@ -5,8 +5,7 @@ import Image from 'next/image';
 const ProfileCard = () => {
   return (
     <section className="relative flex h-[301px] w-full rounded-[10px] bg-white">
-        <div className="profileBanner h-[150px] w-full rounded-t-[10px] md:h-[182px]">
-        </div>
+        <div className="profileBanner h-[150px] w-full rounded-t-[10px] md:h-[182px]"/>
         <div className="absolute left-[13px] top-[115px] h-[70px] w-[70px] md:left-[31px] md:top-[119px] md:h-40 md:w-40">
             <Image
             src={"/img/profile.jpg"}
@@ -26,7 +25,7 @@ const ProfileCard = () => {
             <div className="absolute left-0 top-0 text-xl font-bold text-gray-900">Jane Daniel</div>
             <div className="absolute left-0 top-[32px] text-sm font-normal text-gray-900 opacity-50">Agent</div>
         </div>
-        <div className="absolute left-[207px] top-[245px] inline-flex">
+        <div className="absolute right-2.5 top-[245px] inline-flex md:right-[50px] md:top-[220px]">
             <Button href="/" title="Edit Profile" style="btn-edit-profile" />
         </div>
     </section>
