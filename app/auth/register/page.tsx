7f89fae5dfbe38be415311e19289c765674927ca
Matthew Gajo <matthew.gajo@gmail.com>
2023-08-21 18:03:14 -0400

    Saving before changing branches

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <main className='padding-layout my-12'>
+      <section className='w-full bg-black'>
+        <form className='flex items-center justify-center'>
+          <h1>MORENT</h1>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
