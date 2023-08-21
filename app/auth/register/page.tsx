52049842a36c7b57a18d18589c7e92db02e5be2b
Matthew Gajo <matthew.gajo@gmail.com>
2023-08-21 18:03:26 -0400

    Saving before changing branches

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Page = (props: Props) => {
   return (
     <main className='padding-layout my-12'>
-      <section className='w-full bg-black'>
+      <section className='w-full '>
         <form className='flex items-center justify-center'>
           <h1>MORENT</h1>
         </form>
